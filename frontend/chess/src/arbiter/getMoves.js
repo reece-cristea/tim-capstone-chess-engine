d966bc84ef531b234851f01874243c3cecfbf0b5
Reece Cristea <reece.cristea@mymail.champlain.edu>
2024-03-20 12:28:03 -0700

    implemented bishop, queen and king movement

--- frontend/chess/src/arbiter/getMoves.js
+++ frontend/chess/src/arbiter/getMoves.js
@@ -47,10 +47,6 @@ export const getKnightMoves = (position, rank, file) => {
   ]
 
   legalSquares.forEach(sq => {
-    console.log(rank)
-    console.log(file)
-    console.log(sq[0])
-    console.log(sq[1])
     const square = position?.[rank + sq[0]]?.[file + sq[1]];
     if (square !== undefined && (square.startsWith(ai) || square === '')) {
       moves.push([rank + sq[0], file + sq[1]]);
@@ -58,4 +54,100 @@ export const getKnightMoves = (position, rank, file) => {
   })
 
   return moves
+}
+
+export const getBishopMoves = (position, piece, rank, file) => {
+  const moves = [];
+  const player = piece[0];
+  const ai = player === 'w' ? 'b' : 'w';
+
+  const direction = [
+    [-1,1],
+    [1,1],
+    [-1,-1],
+    [1,-1]
+  ];
+
+  direction.forEach(dir => {
+    for (let i = 1; i < 8; i++) {
+      const x = rank + (i*dir[0])
+      const y = file + (i*dir[1])
+      if(position?.[x]?.[y] === undefined) 
+        break
+      if(position[x][y].startsWith(ai)) {
+        moves.push([x,y])
+        console.log("here" + x + y)
+        break
+      }
+      if(position[x][y].startsWith(player)) {
+        console.log("here" + x + y)
+        break
+      }
+      moves.push([x,y])
+    }
+  })
+  return moves;
+}
+
+export const getQueenMoves = (position, piece, rank, file) => {
+  const moves = [];
+  const player = piece[0];
+  const ai = player === 'w' ? 'b' : 'w';
+
+  const direction = [
+    [-1,1],
+    [1,1],
+    [-1,-1],
+    [1,-1],
+    [-1,0],
+    [1,0],
+    [0,-1],
+    [0,1]
+  ];
+
+  direction.forEach(dir => {
+    for (let i = 1; i < 8; i++) {
+      const x = rank + (i*dir[0])
+      const y = file + (i*dir[1])
+      if(position?.[x]?.[y] === undefined) 
+        break
+      if(position[x][y].startsWith(ai)) {
+        moves.push([x,y])
+        console.log("here" + x + y)
+        break
+      }
+      if(position[x][y].startsWith(player)) {
+        console.log("here" + x + y)
+        break
+      }
+      moves.push([x,y])
+    }
+  })
+  return moves;
+}
+
+export const getKingMoves = (position, piece, rank, file) => {
+  const moves = [];
+  const player = piece[0];
+  const ai = player === 'w' ? 'b' : 'w';
+
+  const direction = [
+    [-1,1],
+    [1,1],
+    [-1,-1],
+    [1,-1],
+    [-1,0],
+    [1,0],
+    [0,-1],
+    [0,1]
+  ];
+
+  direction.forEach(dir => {
+      const x = rank + dir[0]
+      const y = file + dir[1]
+      if(position?.[x]?.[y] !== undefined && (!position[x][y].startsWith(player) || position[x][y].startsWith(ai))) {
+        moves.push([x,y])
+      }
+  })
+  return moves;
 }
