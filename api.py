56962394c0dbfc3e89ad3b2b5016330330e52998
Reece Cristea <reece.cristea@mymail.champlain.edu>
2024-05-06 19:48:59 -0700

    trying to add .h5 file

--- api.py
+++ api.py
@@ -1,9 +1,20 @@
-from flask import Flask
+import os
+from flask import Flask, send_from_directory
 from main import ChessGame
 import json
 from flask_cors import CORS
 
-app = Flask(__name__)
+
+app = Flask(__name__, static_folder='frontend/chess/build')
+
+# Serve React App
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def serve(path):
+    if path != "" and os.path.exists(app.static_folder + '/' + path):
+        return send_from_directory(app.static_folder, path)
+    else:
+        return send_from_directory(app.static_folder, 'index.html')
 CORS(app)
 
 game = ChessGame()
@@ -25,4 +36,4 @@ def move(algebraic_move):
     return json.dumps(ai_move)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0', use_reloader=True, port=5000, threaded=True)
